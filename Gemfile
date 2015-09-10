183613cdf4acdf7fe60b15da7386490aef0e38d8
Renan Lage <renalage@gmail.com>
2015-09-10 18:26:24 -0300

    Initialize repository with CASync plugin

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'clockwork', '~> 1.2.0'
+gem 'delayed_job_active_record', '~> 4.0.3'
---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :casync do
+  name 'CASync Plugin'
+  author 'Renan Lage'
+  description 'This is a plugin for Redmine integration with the CA HelpDesk'
+  version '0.0.1'
+end
